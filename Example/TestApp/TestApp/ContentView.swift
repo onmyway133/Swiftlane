5619c7aa8577669f59cc9a584edf0fbb0c582698
Khoa Pham <onmyway133@gmail.com>
2019-08-21 09:34:33 +0200

    Add TestApp

---
+++ Example/TestApp/TestApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by khoa on 21/08/2019.
+//  Copyright © 2019 PumaSwift. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
