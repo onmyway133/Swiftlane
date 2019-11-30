b6656d027203d91a2871ed909b0f1672413b6557
Khoa Pham <onmyway133@gmail.com>
2019-11-30 23:10:55 +0100

    Remove unused code

--- Example/TestPuma/TestPuma/main.swift
+++ Example/TestPuma/TestPuma/main.swift
@@ -11,19 +11,6 @@ import Puma
 import PumaiOS
 
 func testDrive() {
-//    let automaticSigning = AutomaticSigning(teamId: "T78DK947F2")
-//    let recommendedArchiveOptions = OptionFactory().makeArchiveOptions(name: "TestApp")
-//
-//    let xcodebuildOptions = Xcodebuild.Options(
-//        workspace: nil,
-//        project: "TestApp",
-//        scheme: "TestApp",
-//        configuration: Configuration.release,
-//        sdk: Sdk.iPhone,
-//        signing: .auto(automaticSigning),
-//        usesModernBuildSystem: true
-//    )
-
     run {
         SetVersionNumber {
             $0.versionNumberForAllTargets("1.1")
@@ -47,39 +34,6 @@ func testDrive() {
                 os: Destination.OS.os12_2
             ))
         }
-//        SetVersionNumber(options: .init(buildNumber: "1.1"))
-//        SetBuildNumber(options: .init(buildNumber: "2"))
-//
-//        Build(options: .init(
-//            buildOptions: xcodebuildOptions,
-//            buildsForTesting: true)
-//        )
-//
-//        Test(options: .init(
-//            buildOptions: xcodebuildOptions,
-//            destination: Destination(
-//                platform: Destination.Platform.iOSSimulator,
-//                name: Destination.Name.iPhoneXr,
-//                os: Destination.OS.os12_2
-//            )
-//        ))
-//
-//        Archive(options: .init(
-//            buildOptions: xcodebuildOptions,
-//            archivePath: recommendedArchiveOptions.archivePath
-//        ))
-//
-//        ExportArchive(
-//            options: .init(
-//                exportOptionsPlist: nil,
-//                archivePath: recommendedArchiveOptions.archivePath,
-//                exportPath: recommendedArchiveOptions.exportPath
-//            ),
-//            exportPlist: .init(
-//                teamId: "T78DK947F2",
-//                method: ExportMethod.development
-//            )
-//        )
     }
 }
 
