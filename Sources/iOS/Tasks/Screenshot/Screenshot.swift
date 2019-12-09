70795af5584b28c97f7fc5d3bcd1ed08ae36ae66
Khoa Pham <onmyway133@gmail.com>
2019-12-09 22:13:55 +0100

    Add task Screenshot

---
+++ Sources/iOS/Tasks/Screenshot/Screenshot.swift
@@ -0,0 +1,25 @@
+//
+//  Screenshot.swift
+//  PumaiOS
+//
+//  Created by khoa on 09/12/2019.
+//
+
+import Foundation
+import PumaCore
+
+public class Screenshot: UsesXcodeBuild {
+    public var xcodebuildArguments = [String]()
+
+    public init(_ closure: (Screenshot) -> Void = { _ in }) {
+        closure(self)
+    }
+}
+
+extension Screenshot: Task {
+    public var name: String { "Screenshot" }
+
+    public func run(workflow: Workflow, completion: TaskCompletion) {
+        completion(.failure(PumaError.unknown))
+    }
+}
