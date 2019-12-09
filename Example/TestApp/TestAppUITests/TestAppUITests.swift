d6c04aeab4606eee56320edc3864c45c24b8bc90
Khoa Pham <onmyway133@gmail.com>
2019-12-09 23:01:25 +0100

    Add UITests target to TestApp

---
+++ Example/TestApp/TestAppUITests/TestAppUITests.swift
@@ -0,0 +1,16 @@
+//
+//  TestAppUITests.swift
+//  TestAppUITests
+//
+//  Created by khoa on 09/12/2019.
+//  Copyright © 2019 PumaSwift. All rights reserved.
+//
+
+import XCTest
+
+class TestAppUITests: XCTestCase {
+    func testFirstScreen() {
+        let app = XCUIApplication()
+        app.launch()
+    }
+}
