4626257e34203ba10d02b35d0615f47a908d889e
Khoa Pham <onmyway133@gmail.com>
2019-12-28 21:42:15 +0100

    Add UploadApp

---
+++ Sources/iOS/Library/Tools/Altool/Altool.swift
@@ -0,0 +1,21 @@
+//
+//  Altool.swift
+//  PumaiOS
+//
+//  Created by khoa on 28/12/2019.
+//
+
+import Foundation
+import PumaCore
+
+public struct Altool {
+    var arguments: [String] = []
+
+    func run(workflow: Workflow) throws {
+        try CommandLine().runBash(
+            workflow: workflow,
+            program: "xcrun",
+            arguments: ["altool"] + arguments
+        )
+    }
+}
---
+++ Sources/iOS/Tasks/UploadApp/UploadApp.swift
@@ -0,0 +1,43 @@
+//
+//  UploadApp.swift
+//  PumaiOS
+//
+//  Created by khoa on 28/12/2019.
+//
+
+import Foundation
+import PumaCore
+
+public class UploadApp {
+    public var name: String = "Upload app"
+    public var isEnabled = true
+    public var altool = Altool()
+
+    public init(_ closure: (UploadApp) -> Void = { _ in }) {
+        closure(self)
+    }
+}
+
+extension UploadApp: Task {
+    public func run(workflow: Workflow, completion: TaskCompletion) {
+        handleTryCatch(completion) {
+            try altool.run(workflow: workflow)
+        }
+    }
+}
+
+public extension UploadApp {
+    func authenticate(username: String, password: String) {
+        altool.arguments.append(contentsOf: [
+            "--username", username,
+            "--password", password
+        ])
+    }
+
+    func upload(ipaPath: String) {
+        altool.arguments.append(contentsOf: [
+            "--upload-app",
+            "--file", ipaPath
+        ])
+    }
+}
