ad71a89a250973b9a32a0df7f0bf6115a0b1c023
besarismaili <b3sar11@gmail.com>
2020-01-03 22:05:36 +0100

    CustomConsole add

---
+++ Sources/Core/Library/CustomConsole.swift
@@ -0,0 +1,12 @@
+//
+//  CustomConsole.swift
+//  Basic
+//
+//  Created by Besar Ismaili on 03/01/2020.
+//
+
+import Foundation
+
+public struct CustomConsole {
+    public static let pumaLogoAscii
+}
