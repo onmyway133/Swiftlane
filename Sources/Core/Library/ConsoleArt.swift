2034cd58b4281d1abb29f1268114212d13bd0e6b
besarismaili <b3sar11@gmail.com>
2020-01-13 20:49:44 +0100

    make pumaLogoAscii smaller

--- Sources/Core/Library/ConsoleArt.swift
+++ Sources/Core/Library/ConsoleArt.swift
@@ -10,45 +10,34 @@ import Foundation
 public struct ConsoleArt {
     
     public static let pumaLogoAscii = """
-                                                                                         
-       ...''...                                           ...'''..
-     .':cllllc:,..            .............            ..,:clloll:,.
-    .:lllc::cllll:'..     ..';:clllllllllc:;'..     ..':llllc;;clll:.
-   .;lll;.....;clllc:,...,:lllllllllllllllllll:,...,:clllc;..  .;lll;.
-   'lll;.      .';clllc:clllllllllllllllllllllllc:clllc;'.      .:lll'
-  .;lll'          .'clllllllllllllllllllllllllllllllc'..         'cll;.
-  .:llc.         ..;clllllllllllllllllllllllllllllllc;..         .cll:.
-  .:llc.       ..;clllllllllllllllllllllllllllllllllllc;.        .colc.
-  .:olc.      .;clllllllllllllllllllllllllllllllllllllllc;.      .cll:.
-  .:llc.   ..;clllllllllllllllllllllllllllllllllllllllllllc,..   .cll:.
-  .:llc. ..;clllllllllllllllllllllllllllllllllllllllllllllllc;.. .cll:.
-  .:llc'.;clllllllllllllllllllllllllllllllllllllllllllllllllllc;..col:.
-  .:ollcclllllllllllllllllllllllllllllllllllllllllllllllllllllllccllo:.
-  .:olllllllllllllllllccclllllllllllllllllllllllcccclllllllllllllllll:.
-  .:ollllllllccc:;,,......':lllllllllllllllllc,......',,;cccllllllllo:.
-  .clllllllllc:;;;,.    .. .:llllllllllllllll, ...   .';;;;:lllllllloc.
- .:lllllllllllolol,.    .,..;lllllllllllllloc..,'     .colllllllllllll:.
- 'lollllllllllllllc'....,c'.;lllllllllllllllc..::.....;llllllllllllllll'
-.;llllllllllllllllllc::cll'.:olllllllllllllll'.:ol:::clllllllllllllllll;.
-.;lllllllllllllllllllllloc.'lllllllllllllllll:.,lllllllllllllllllllllll;.
- ,lllllllllllllllllllllll,':olllllllllllllllll,'colllllllllllllllllllll,
- .:olllllllllllllllllllo:':lllllllllllllllllllc,,lllllllllllllllllllll:.
-  'clllllllllllllllllllc;:lllllllllllllllllllllc;:lllllllllllllllllllc'
-  .'cllllllllllllllllllcclllllllllllllllllllllllccllllllllllllllllllc'.
-    .:lllllllllllllllllllllc;....',;;;,'...';llllllllllllllllllllll;.
-     .':lllllllllllclllllll,.               .;lllllllclllllllllll:'.
-       .':llllllllc:cllllllc,'..         ..';cllllll::cllllllll:'.
-         .':lllllc:llllllllllll:..     .'clllllllllllc:clllll:'.
-           ..:lllc:clllllllllllll;.  ..:lllllllllllllc:clll:'.
-             .';cc::lllllllllllllo:. .colllllllllllll::lc;..
-               ..;::clllloolllcc;,.....,;:clllolllllcc:;..
-                 ..,;;;,,''''....',:::,''''',,;;:::::;..
-                   .':cc:;;;;:ccllllllllllcccccclll:'.
-                     .':lllllllllllllllllllllllll:'.
-                       ..;clllllllllllllllllllc;..
-                          .',:clllllllllllc:,'.
-                             ....'''''''....
 
+    ..,;;,'..          .......          ..';:;;'.
+   .:ll::clc;'..  ..',;::cccc:;,'..  ..';clc;:ll:.
+  .:oc'...';clc;'';clllllllllllllc;'';clc;'. .'cl:.
+  'll,.     .':lllllllllllllllllllllll:'.     .,ll'
+ .,ll'      .':lllllllllllllllllllllll:'.      'll,.
+ .,ll'    .':lllllllllllllllllllllllllll:'.    'll,.
+ .,ll'  .':lolllllllllllllllllllllllllllll:'.  'll,
+ .,ll'.':lllllllllllllllllllllllllllllllllll:'.'ll,.
+ .,ll::lllllllllllllllllllllllllllllllllllllll::ll,.
+ .,lllllllllllc::::lllllllllllllllc:::ccllllllllll,.
+ .;lllllllc::,......:lllllllllloc'.....';::cllllll;.
+ .cllllllllll;.  .'.,llllllllllo:...   'clllllllllc.
+.;lllllllllllc,..;:',llllllllllo:.;:'.':lllllllllll;.
+.;lllllllllllllllo:':ollllllllloc';llllllllllllllll;.
+.,llllllllllllllll;,lllllllllllll:,clllllllllllllll,.
+ .cllllllllllllll:;cllllllllllllll::llllllllllllll:.
+  .:lllllllllllllcllllcclloolccllllclllllllllllll:.
+   .;clllllllllllllc,....'''....,cllllllllllllll;.
+    ..;clllllccllllc,..      ...,cllllcclllllc;.
+      ..;cllccclllllllc'.   .,clllllllcccllc;..
+         .;cc:clllllllll:. .:lllllllllc:cc,.
+          ..,;:ccllllc:;,...,;:cllllcc::,..
+             .';:;;,,,,,;:c:;,,;;:::c:,.
+              ..;cllllllllllloolllll;..
+                ..,:lloolllllllll:,.
+                   ..',;;:::;;,'..
+                         ..
 """
     
     public static let pumaTitleAscii = """
