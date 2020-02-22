dd23903fad39266ebd09036b5b9fc4fcf7135a81
besarismaili <b3sar11@gmail.com>
2020-02-22 23:09:15 +0100

    APIProvider add from appstoreconnect-swift-sdk library

---
+++ Sources/iOS/Vendors/AppstoreConnectApi/Result.swift
@@ -0,0 +1,37 @@
+extension Result {
+    
+    /// Returns `true` if the result is a success, `false` otherwise.
+    public var isSuccess: Bool {
+        switch self {
+        case .success:
+            return true
+        case .failure:
+            return false
+        }
+    }
+
+    /// Returns `true` if the result is a failure, `false` otherwise.
+    public var isFailure: Bool {
+        return !isSuccess
+    }
+
+    /// Returns the associated value if the result is a success, `nil` otherwise.
+    public var value: Success? {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure:
+            return nil
+        }
+    }
+
+    /// Returns the associated error value if the result is a failure, `nil` otherwise.
+    public var error: Error? {
+        switch self {
+        case .success:
+            return nil
+        case .failure(let error):
+            return error
+        }
+    }
+}
