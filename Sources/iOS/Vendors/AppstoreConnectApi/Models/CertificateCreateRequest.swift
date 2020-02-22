a8ccc59c0ca8c3f641df8cbf53ad359bec68cfd3
besarismaili <b3sar11@gmail.com>
2020-02-22 23:20:55 +0100

    Models add

---
+++ Sources/iOS/Vendors/AppstoreConnectApi/Models/CertificateCreateRequest.swift
@@ -0,0 +1,31 @@
+//
+//  CertificateCreateRequest.swift
+//  AppStoreConnect-Swift-SDK
+//
+//  Created by Patrick Balestra on 12/15/19.
+//
+
+/// A request containing a single resource.
+public struct CertificateCreateRequest: Encodable {
+    
+    public struct Data: Encodable {
+
+        /// The resource's attributes.
+        public let attributes: CertificateCreateRequest.Data.Attributes?
+
+        /// The resource type.
+        public let type: String = "certificates"
+    }
+    
+    /// The resource data.
+    public let data: Data
+}
+
+// MARK: CertificateCreateRequest.Data
+extension CertificateCreateRequest.Data {
+
+    public struct Attributes: Encodable {
+        public let certificateType: CertificateType
+        public let csrContent: String
+    }
+}
